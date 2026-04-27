b2555714b12500fefae76b32248d5f74d363fa4d
ludo <ludovic.brot@gmail.com>
2026-04-27 15:59:24 +0200

    initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/com/hopeful117/cv_analyzer/CvAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopeful117.cv_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
